84950b32a0fd71dff575ebbb07f278de145933c6
changdy <11>
2018-05-29 20:41:26 +0800

    完善 kafka 发送消息

--- spring-boot-demo/src/main/java/com/changdy/springboot/scheduling/MessageSender.java
+++ spring-boot-demo/src/main/java/com/changdy/springboot/scheduling/MessageSender.java
@@ -1,17 +1,20 @@
 package com.changdy.springboot.scheduling;
 
 import com.changdy.springboot.bean.RedisConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.integration.kafka.support.KafkaHeaders;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.integration.support.MessageBuilder;
 
-import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 
 @Component
+@Slf4j
 public class MessageSender {
 
     @Autowired
@@ -20,7 +23,7 @@ public class MessageSender {
     private StringRedisTemplate stringRedisTemplate;
 
     @Autowired
-    private MessageChannel inputToKafka;
+    private MessageChannel kafkaTopicChannel;
 
     @Scheduled(fixedRate = 2000)
     public void sendRedisMessage() {
@@ -29,7 +32,13 @@ public class MessageSender {
 
     @Scheduled(fixedRate = 2000)
     public void sendKafkaMessage() {
-        inputToKafka.send(MessageBuilder.withPayload("Message-" + LocalDateTime.now()).build());
+        String topicClient = "wshuttle-client";
+        String topicWsccc = "wshuttle-wsccc";
+        boolean send;
+        send = kafkaTopicChannel.send(MessageBuilder.withPayload(topicClient + LocalTime.now()).setHeader(KafkaHeaders.TOPIC, topicClient).build());
+        log.info(topicClient + ": {}", send);
+        send = kafkaTopicChannel.send(MessageBuilder.withPayload(topicWsccc + LocalTime.now()).setHeader(KafkaHeaders.TOPIC, topicWsccc).build());
+        log.info(topicWsccc + ":{}", send);
     }
 
 }
